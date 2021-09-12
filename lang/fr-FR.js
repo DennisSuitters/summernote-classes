8111d164ae3a6deb669ee4be13daba532586c81d
michael <michael@cadot.info>
2021-09-12 10:11:29 +0100

    add lang fr

---
+++ lang/fr-FR.js
@@ -0,0 +1,9 @@
+$.extend(true, $.summernote.lang, {
+    'fr-FR': {
+        classes: {
+            a: ['bouton', 'ligne', 'primary', 'secondary', 'danger'],
+            img: ['taille adaptative', 'à gauche', 'centré', 'à droite', 'arrondis', 'en circle', 'petite ombre', 'ombre', 'grande ombre', 'miniature'],
+            p: ['centré', 'à droite', 'texte justifié', 'texte non débordant', 'text débordant', 'texte tronqué', 'texte cassé', 'en minuscule', 'en majuscule', 'majuscule', 'monospace', 'Léger', 'encadré'],
+        }
+    }
+});
